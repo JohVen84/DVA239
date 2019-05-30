72829f1926486c5d18d9600628e2da9cec86ded6
Johan Vennberg <jv222gw@student.lnu.se>
2019-05-30 10:16:56 +0200

    LabA updated, LabB skeleton

--- Challenge Labs/Challenge_Lab_A.bash
+++ Challenge Labs/Challenge_Lab_A.bash
@@ -1,7 +1,8 @@
 #!/bin/bash
 # Author: Johan Vennberg
 
-# Change dir to root
+# Switch to root user & change dir to root
+su -
 cd /
 
 # Create directories for each department
@@ -28,3 +29,28 @@ useradd -g sales -d /sales sales2
 
 useradd -g is -d /is is1
 useradd -g is -d /is is2
+
+# Change owner of each directory
+
+chown engineering_admin:engineering /engineering
+chown sales_admin:sales /sales
+chown is_admin:is /is
+
+#Change Permission & set sticky bit
+chmod 1770 engineering
+chmod 1770 sales
+chmod 1770 is
+
+#setguid
+chmod g+s engineering
+chmod g+s sales
+chmod g+s is
+
+#Create files in each department directory
+
+touch /engineering/doc.txt
+echo “This file contains confidential information for the department.” > /engineering/doc.txt
+touch /sales/doc.txt
+echo “This file contains confidential information for the department.” > /sales/doc.txt
+touch /is/doc.txt
+echo “This file contains confidential information for the department.” > /is/doc.txt
--- Challenge Labs/Challenge_Lab_B.bash
+++ Challenge Labs/Challenge_Lab_B.bash
@@ -1,1 +1,19 @@
-#!/bin/bash 
+#!/bin/bash
+#Author Johan Vennberg
+
+group=$1
+user=$2
+
+groupadd $1
+
+useradd -g $1 $2
+
+pass = "password"
+
+passwd $2
+
+cd /
+mkdir $2
+
+chown $2:$1 /$2
+chmod 1770 $2
