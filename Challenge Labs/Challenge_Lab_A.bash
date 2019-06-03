fed84ddfaffdc0729c52fd82bf8370614e3ca614
Johan <jv222gw@student.lnu.se>
2019-06-03 09:34:28 +0200

    Lab C done

--- Challenge Labs/Challenge_Lab_A.bash
+++ Challenge Labs/Challenge_Lab_A.bash
@@ -37,9 +37,9 @@ chown sales_admin:sales /sales
 chown is_admin:is /is
 
 #Change Permission & set sticky bit
-chmod 1770 engineering
-chmod 1770 sales
-chmod 1770 is
+chmod -R 1770 engineering
+chmod -R 1770 sales
+chmod -R 1770 is
 
 #setguid
 chmod g+s engineering
--- Challenge Labs/Challenge_Lab_C.bash
+++ Challenge Labs/Challenge_Lab_C.bash
@@ -1,1 +1,18 @@
-#!/bin/bash 
+#!/bin/bash
+cd /
+
+mkdir ~/archive
+
+#Bad solluiton? I tried things such as tar -cvf ~/archive/log.tar -C var/log . *.log  but either manage to get the whole directory or only the files with its pathname
+#I think I could get som good feedback for this part
+cd var/log
+tar -cvf ~/archive/log.tar *.log
+
+#List the conttents of the archivefile
+tar -tf ~/archive/log.tar
+
+#Extract to the backup directory
+tar -xvf ~/archive/log.tar -C ~/backup
+
+cd  ~/backup
+ls
