d37add8f3c7e356f0bb50478bd577c1cec8fc640
Johan Vennberg <jv222gw@student.lnu.se>
2019-06-05 19:12:14 +0200

    Update labD

--- Challenge Labs/Challenge_Lab_D.bash
+++ Challenge Labs/Challenge_Lab_D.bash
@@ -1,11 +1,4 @@
 #!/bin/bash
 
 
-cat /etc/services
-
-cut -f1 /etc/services | grep -v '^#' |sort --unique > uniqueservices.txt 
-
-
-
-
-cut -f1 /etc/services | grep -v '^#' |grep '^$'|sort --unique > uniqueservices.txt
+cut -f1 /etc/services | grep -v '^#' |sort --unique > uniqueservices.txt && wc -l uniqueservices.txt
